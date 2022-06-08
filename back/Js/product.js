dab0442bc3c919df101811b018be5b937b402adb
Hugo Esquinazi <hugo.esquinazi@gmail.com>
2022-06-08 14:02:09 +0200

    ajout product.js

--- back/Js/product.js
+++ back/Js/product.js
@@ -1,3 +1,28 @@
-/* recup parametre
-    ajouter soustraire produit
-    conf
+
+(function(){
+    const productId = getProductId()
+    const product = await getProduct(productId)
+    hydrateProduct(product)
+})()
+
+function getProductId(){
+    return new URL(location.href).searchParams.get("item__img")
+}
+
+function getProduct(){
+    return fetch("http://localhost:3000/api/products/${productId}")
+    .then(function(httpBodyResponse){
+        return httpBodyResponse.json()
+    })
+    .then(function(products){
+        return products
+    })
+    .catch(function(error){
+        alert(error)
+    })
+}
+
+function hydrateProduct(){
+    document.getElementsById("_id").textContent = product.title
+    document.getElementById("_id").textContent = product.body
+}
